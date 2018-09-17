955934f9af04af580712057e97b45fc74cafe44b
Gina Fedder <ginafedder@yahoo.com>
2018-09-17 23:35:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,6 @@ def find_the_cheese(strings)
   array << strings
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    cheese.include?(cheese_types)
+    cheese_types.include?(cheese)
   end
 end
